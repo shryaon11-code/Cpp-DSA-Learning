981605aa4ccc469eb86c069e323f379cf9081490
Shreya singh <shryaon11@gmail.com>
2025-08-23 02:21:52 +0530

    added code for lecture 3

---
+++ Lecture_03_Start_C++_from_Zero_and_Write_Your_First_Program/code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+
+    int age = 20;
+    char grade = 'A';
+    float pi = 3.14f;
+    double largeValue = 12345.6789;
+    bool isCodingFun = true;
+
+    cout << "Age: " << age << endl;
+    cout << "Grade: " << grade << endl;
+
+    return 0;
+}
