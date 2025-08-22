b8341fc2b7b43303284225abaf18a6a8fd9d7965
Shreya singh <shryaon11@gmail.com>
2025-08-23 02:14:13 +0530

    added code for lecture 2

--- Lecture_02_Introduction_to_FlowCharts_and_PseudoCode_in_Programming/code.cpp
+++ Lecture_02_Introduction_to_FlowCharts_and_PseudoCode_in_Programming/code.cpp
@@ -1,13 +1,118 @@
-// Lecture 02: Introduction to FlowCharts and PseudoCode in Programming
-// Add your implementations per problem in separate files or here.
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+// 1. Sum of two numbers
+void sumOfTwo() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "Sum = " << a + b << endl;
+}
 
-    // TODO: write demo / tests for this lecture's concepts
-    cout << "Lecture 02: Introduction to FlowCharts and PseudoCode in Programming\n";
+// 2. Average of two numbers
+void averageOfTwo() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "Average = " << (a + b) / 2.0 << endl;
+}
+
+// 3. Cube and Square
+void cubeAndSquare() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "Square = " << n * n << ", Cube = " << n * n * n << endl;
+}
+
+// 4. Even or Odd
+void evenOdd() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    if (n % 2 == 0)
+        cout << n << " is Even." << endl;
+    else
+        cout << n << " is Odd." << endl;
+}
+
+// 5. Positive or Negative
+void positiveNegative() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    if (n > 0)
+        cout << n << " is Positive." << endl;
+    else if (n < 0)
+        cout << n << " is Negative." << endl;
+    else
+        cout << "Number is Zero." << endl;
+}
+
+// 6. Print a number 10 times
+void printTenTimes() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    for (int i = 0; i < 10; i++)
+        cout << num << " ";
+    cout << endl;
+}
+
+// 7. Print n natural numbers
+void printNaturalNumbers() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        cout << i << " ";
+    cout << endl;
+}
+
+// 8. Sum of n natural numbers
+void sumNaturalNumbers() {
+    int n, sum = 0;
+    cout << "Enter n: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        sum += i;
+    cout << "Sum = " << sum << endl;
+}
+
+// 9. Prime number check
+void primeCheck() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    bool isPrime = true;
+    if (n <= 1) isPrime = false;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime)
+        cout << n << " is Prime." << endl;
+    else
+        cout << n << " is Not Prime." << endl;
+}
+
+int main() {
+    cout << "Lecture 2: Flowcharts & PseudoCode Examples\n" << endl;
+
+    sumOfTwo();
+    averageOfTwo();
+    cubeAndSquare();
+    evenOdd();
+    positiveNegative();
+    printTenTimes();
+    printNaturalNumbers();
+    sumNaturalNumbers();
+    primeCheck();
+
     return 0;
 }
