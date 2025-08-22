52a37b05d5365bb00f34422eb53322e9ce54d83d
Shreya singh <shryaon11@gmail.com>
2025-08-23 02:06:04 +0530

    added code for lecture 1

--- Lecture_01_Introduction_To_Programming_for_Beginners/code.cpp
+++ Lecture_01_Introduction_To_Programming_for_Beginners/code.cpp
@@ -1,13 +1,7 @@
-// Lecture 01: Introduction To Programming for Beginners
-// Add your implementations per problem in separate files or here.
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    // TODO: write demo / tests for this lecture's concepts
-    cout << "Lecture 01: Introduction To Programming for Beginners\n";
+    cout << "Hello, World! Welcome to C++ Programming." << endl;
     return 0;
 }
